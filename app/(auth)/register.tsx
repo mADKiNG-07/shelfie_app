aca1b03eb3fa0a15943ff9202e32e68d96066663
Jordan Williams <jordanmhizcleo@gmail.com>
2026-01-10 23:33:29 +0100

    feat: enhance registration screen with themed components and navigation link

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,11 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
+import ThemedView from "../../components/ThemedView";
+import Spacer from "../../components/Spacer";
+import ThemedText from "../../components/ThemedText";
+import { index_styles } from "..";
+import { Link } from "expo-router";
 
 const register = () => {
   return (
-    <View>
-      <Text>register</Text>
-    </View>
+    <ThemedView style={index_styles.container}>
+      <Spacer />
+
+      <ThemedText title={true} style={index_styles.boldTitle}>
+        Register to your account
+      </ThemedText>
+
+      <Spacer height={100} />
+
+      <Link href="/login">
+        <ThemedText style={{ textAlign: "center" }}>
+          Already have an account? Login
+        </ThemedText>
+      </Link>
+    </ThemedView>
   );
 };
 
